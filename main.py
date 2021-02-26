cbe1a339c8dd1bbf16ae47ddf5b837ef027aa6cf
mfkarch <mohammadfarankhalid@gmail.com>
2021-02-26 19:45:12 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import cv2
+import time
+from imutils.video import FPS
+
+CONFIDENCE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.4
+COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+fps = FPS().start()
+# class_names = []
+with open("input/coco.names", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+print(class_names)
+vc = cv2.VideoCapture("rtsp://admin:admin123@192.168.100.9:554/cam/realmonitor?channel=2&subtype=0")
+
+net = cv2.dnn.readNet("input/yolov4.weights", "input/yolov4.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(416, 416), scale=1 / 255)
+
+while cv2.waitKey(1) < 1:
+    (grabbed, frame) = vc.read()
+    if not grabbed:
+        exit()
+
+    start = time.time()
+    classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    end = time.time()
+    print(classes, scores, boxes)
+
+    start_drawing = time.time()
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        print(classid, ", ", score)
+        if classid[0] == 0:
+            label = "%s : %f" % (class_names[classid[0]], score)
+            cv2.rectangle(frame, box, color, 2)
+            cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    end_drawing = time.time()
+
+    fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (
+        1 / (end - start), (end_drawing - start_drawing) * 1000)
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.imshow("detections", frame)
+    fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+# release the file pointers
+print("[INFO] cleaning up..")
+vc.release()
